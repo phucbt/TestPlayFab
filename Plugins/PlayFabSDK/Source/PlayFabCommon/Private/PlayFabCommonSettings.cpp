ca5cd1b6950be01e5599926bbc27333d171eabba
phucbt <99398478+phucbt@users.noreply.github.com>
2022-10-17 14:19:12 +0700

    Test Data

---
+++ Plugins/PlayFabSDK/Source/PlayFabCommon/Private/PlayFabCommonSettings.cpp
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////
+// Copyright (C) Microsoft. 2018. All rights reserved.
+//////////////////////////////////////////////////////
+
+
+#include "PlayFabCommonSettings.h"
+
+namespace PlayFabCommon
+{
+    const FString PlayFabCommonSettings::sdkVersion = "1.101.220926";
+    const FString PlayFabCommonSettings::buildIdentifier = "adobuild_unrealmarketplaceplugin_142";
+    const FString PlayFabCommonSettings::versionString = "UE4MKPL-1.101.220926";
+
+    FString PlayFabCommonSettings::clientSessionTicket;
+    FString PlayFabCommonSettings::entityToken;
+    FString PlayFabCommonSettings::photonRealtimeAppId;
+    FString PlayFabCommonSettings::photonTurnbasedAppId;
+    FString PlayFabCommonSettings::photonChatAppId;
+}
